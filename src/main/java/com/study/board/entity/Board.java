2a12c87dc3ea6a0b5f5da4eb13d30b2509d641a4
skoh <osk2001@empal.com>
2022-02-08 06:51:23 +0900

    Board_han_만들기중

---
+++ src/main/java/com/study/board/entity/Board.java
@@ -0,0 +1,52 @@
+package com.study.board.entity;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String title;
+
+    public Board() {
+    }
+
+    private String content;
+
+    public Board(Integer id, String title, String content) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
---
+++ src/main/java/com/study/board/service/BoardService.java
@@ -0,0 +1,31 @@
+package com.study.board.service;
+
+import com.study.board.entity.Board;
+import com.study.board.repository.BoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BoardService {
+    @Autowired
+    private BoardRepository boardRepository;
+
+    public void write(Board board){
+        boardRepository.save(board);
+    }
+
+    public List<Board> boardList(){
+        return boardRepository.findAll();
+    }
+
+    public Board boardView(Integer id){
+        return boardRepository.findById(id).get();
+    }
+
+    public void boardDelete(Integer id){
+        boardRepository.deleteById(id);
+    }
+
+}
